1867c4ced78c4830bfa31482de1bfff194e5f1b8
GQL77 <128496681+GQL77@users.noreply.github.com>
2024-09-04 16:22:07 +0800

    Update App.java
    修改了

--- src/main/java/com/iworkcloud/App.java
+++ src/main/java/com/iworkcloud/App.java
@@ -20,10 +20,11 @@ public class App
 {
     public static void main( String[] args )
     {
+		System.out.println("程序正在asd运行...");
 		SpringApplication.run(App.class, args);
 		System.out.println("程序正在运行...");
 		System.out.println("程序正在运行...");
-		System.out.println("程序正在运行...");
+		System.out.println("程");
 		System.out.println("程序正在运行...");
     }
 }
