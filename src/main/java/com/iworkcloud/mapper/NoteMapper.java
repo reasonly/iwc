1c393160803269bdb3a5dc541b374cf79ab149bc
wowomow <Name2431273239@outlook.com>
2024-09-05 11:09:40 +0800

    note
    note

---
+++ src/main/java/com/iworkcloud/mapper/NoteMapper.java
@@ -0,0 +1,19 @@
+package com.iworkcloud.mapper;
+
+import com.iworkcloud.pojo.entity.Note;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 
+* Title: UserDao
+* Description:
+* 用户数据接口 
+* Version:1.0.0
+ */
+@Mapper
+public interface NoteMapper extends BaseMapper<Note> {
+
+    Note findByUserId(Note note);
+    Note findByAdministratorId(Note note);
+
+}
---
+++ src/main/java/com/iworkcloud/service/impl/NoteServiceImpl.java
@@ -0,0 +1,53 @@
+package com.iworkcloud.service.impl;
+
+import com.iworkcloud.mapper.BaseMapper;
+import com.iworkcloud.mapper.NoteMapper;
+import com.iworkcloud.pojo.entity.Note;
+import com.iworkcloud.pojo.entity.User;
+import com.iworkcloud.service.NoteService;
+import com.iworkcloud.util.MD5Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteServiceImpl extends BaseServiceImpl<Note> implements NoteService {
+    //mybatis
+    @Autowired
+    private NoteMapper noteMapper;
+
+    @Override
+    protected BaseMapper<Note> getMapper() {
+        return this.noteMapper;
+    }
+
+    /**
+     * @param note 用户实体
+     * @return 查询结果
+     */
+    @Override
+    public Boolean findByUserId(Note note) {
+        try {
+            System.out.println("findByUserId");
+            Note note1 = noteMapper.findByUserId(note);
+            return note1 != null;
+        } catch (Exception e) {
+            System.out.println("查询失败!");
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    @Override
+    public Boolean findByAdministratorId(Note note) {
+        try {
+            System.out.println("findByAdministratorId");
+            Note note1 = noteMapper.findByAdministratorId(note);
+            return note1 != null;
+        } catch (Exception e) {
+            System.out.println("查询失败!");
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
