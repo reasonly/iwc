5e39acdf86b3e2a189861b46e49831926782944d
reasonly <2032507897@qq.com>
2024-09-11 14:18:38 +0800

    9.11 注释

--- src/main/java/com/iworkcloud/mapper/ProjectMapper.java
+++ src/main/java/com/iworkcloud/mapper/ProjectMapper.java
@@ -14,11 +14,12 @@ public interface ProjectMapper extends BaseMapper<Project>{
     List<Project> findByUserId(int id);
     //根据条件和用户id查询
     List<Project> findByListEntityAndUserId(@Param("Project")Project project,@Param("id") Integer id);
+    //根据项目实体查询项目列表
     List<Project> findByListEntity(@Param("Project")Project project);
-
+    //为指定项目添加用户
     boolean addUser(Integer projectId, List<Integer> userIdList);
-
+    //查询不在指定项目中的用户列表
     List<User> findUsersNotInProject(Integer projectId, String authority);
-
+    //查询在指定项目中的用户列表
     List<User> findUsersInProject(Integer projectId, String authority);
 }
